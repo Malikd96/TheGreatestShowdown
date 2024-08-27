43ff9167a87088d8885484f63e76fb6b20c819e0
Malikd96 <96malikdavis@gmail.com>
2024-08-26 20:39:41 -0400

    Task1and2

---
+++ Task1.py
@@ -0,0 +1,18 @@
+# The Greatest Showdown
+#Task 1: Identify the Greatest. Write a Python program that 
+# asks the user to enter three numbers. Your code should then 
+# identify and print out the largest number among the three.
+
+
+num1 = float(input("Enter the first number:"))
+num2 = float(input("Enter the second number:"))
+num3 = float(input("Enter the third number:"))
+
+
+if num1 >= num2 and num1 >= num3:
+    largest = num1
+elif num2 >= num1 and num2 >= num3:
+    largest = num2
+else:
+    largest = num3
+print(f"The largest number is:{largest}")
---
+++ Task2.py
@@ -0,0 +1,16 @@
+#Task 2: Identify the Smallest Improve upon your code from 
+# Task 1 to also determine the smallest number among the 
+# three and print it out.
+
+num1 = float(input("Enter the first number:"))
+num2 = float(input("Enter the second number:"))
+num3 = float(input("Enter the third number:"))
+
+
+if num1 <= num2 and num1 <= num3:
+    smallest = num1
+elif num2 <= num1 and num2 <= num3:
+    smallest = num2
+else:
+    smallest = num3
+print(f"The smallest number is:{smallest}")
